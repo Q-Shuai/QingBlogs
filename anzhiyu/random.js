4d73b0603af20e59c314378a584585a0a3b85998
QingShuai <95750262+Q-Shuai@users.noreply.github.com>
2026-07-10 16:28:24 +0800

    Site updated: 2026-07-10 16:28:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/10/hello-world/","2026/07/10/模板-template/"];function toRandomPost(){
+var posts=["2026/07/10/模板-template/","2026/07/10/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
