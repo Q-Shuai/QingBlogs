6b5677b3783b1c8e6833201a9d82a09e34901741
QingShuai <95750262+Q-Shuai@users.noreply.github.com>
2026-07-16 20:10:47 +0800

    Site updated: 2026-07-16 20:10:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/13/SEO基础入门教程/","2024/07/11/一口气彻底教会你剪映专业版/"];function toRandomPost(){
+var posts=["2026/03/16/网页开发三件套-Html-CSS-Javascrit-Tailwind-基础语法速览/","2026/03/13/SEO基础入门教程/","2024/07/11/一口气彻底教会你剪映专业版/"];function toRandomPost(){
     pjax.loadUrl('/QingBlogs/'+posts[Math.floor(Math.random() * posts.length)]);
   };
