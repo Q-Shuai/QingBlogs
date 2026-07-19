bdb8ca6ee69ae0e329f869d6787bff88e33ae898
QingShuai <95750262+Q-Shuai@users.noreply.github.com>
2026-07-19 20:12:54 +0800

    Site updated: 2026-07-19 20:12:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/13/SEO基础入门教程/","2026/05/18/Globax-AI-—-外贸-AI-工具站-项目详解/","2026/03/17/建站人员必修课：HTML-CSS-JS-Tailwind学习笔记（LiveCanvas-实战篇）/","2024/07/11/一口气彻底教会你剪映专业版/","2026/03/16/网页开发三件套-Html-CSS-Javascrit-Tailwind-基础语法速览/"];function toRandomPost(){
+var posts=["2026/05/18/Globax-AI-—-外贸-AI-工具站-项目详解/","2025/12/12/WordPress建站前的准备任务/","2026/03/13/SEO基础入门教程/","2024/07/11/一口气彻底教会你剪映专业版/","2026/03/17/建站人员必修课：HTML-CSS-JS-Tailwind学习笔记（LiveCanvas-实战篇）/","2026/03/16/网页开发三件套-Html-CSS-Javascrit-Tailwind-基础语法速览/"];function toRandomPost(){
     pjax.loadUrl('/QingBlogs/'+posts[Math.floor(Math.random() * posts.length)]);
   };
